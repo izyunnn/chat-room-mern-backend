623253347ae59022c6e3f9186c759ebaea1bf82c
izyunnn <queen85579@gmail.com>
2022-06-10 10:42:47 +0800

    add some new files

---
+++ connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connect to Mongo Altas");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
